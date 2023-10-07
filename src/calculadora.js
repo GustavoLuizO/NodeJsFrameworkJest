37fa0a8bd4ee751ff4c8ab554652314fdd34f300
gustavo.oliveira <glogustavo4321@gmail.com>
2023-10-07 18:48:25 -0300

    Create calculadora.js

---
+++ src/calculadora.js
@@ -0,0 +1,23 @@
+class Calculadora {
+    soma(a, b) {
+      return a + b;
+    }
+  
+    subtracao(a, b) {
+      return a - b;
+    }
+  
+    multiplicacao(a, b) {
+      return a * b;
+    }
+  
+    divisao(a, b) {
+      if (b === 0) {
+        throw new Error('Divisão por zero não é permitida.');
+      }
+      return a / b;
+    }
+  }
+
+  module.exports = Calculadora;
+  
