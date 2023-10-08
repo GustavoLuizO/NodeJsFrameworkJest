229b8f9b36fc307d3ddb5852d40960803acd8c0d
gustavo.oliveira <glogustavo4321@gmail.com>
2023-10-08 18:42:08 -0300

    Adding tests using "theory"

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -7,27 +7,89 @@ describe('Calculadora', () => {
     calculadora = new Calculadora();
   });
 
-  describe('soma', () => {
+  describe('soma-fact', () => {
     test('deve somar dois números corretamente', () => {
-      expect(calculadora.soma(2, 3)).toBe(5);
+      const a = 2;
+      const b = 3;
+      const resultado = 5;
+      expect(calculadora.soma(a, b )).toBe(resultado);
     });
   });
 
-  describe('subtracao', () => {
+  describe('subtracao-fact', () => {
     test('deve subtrair dois números corretamente', () => {
-      expect(calculadora.subtracao(5, 3)).toBe(2);
+      const a = 4;
+      const b = 3;
+      const resultado = 1;
+      expect(calculadora.subtracao(a, b)).toBe(resultado);
     });
   });
 
-  describe('multiplicacao', () => {
+  describe('multiplicacao-fact', () => {
     test('deve multiplicar dois números corretamente', () => {
-      expect(calculadora.multiplicacao(2, 3)).toBe(6);
+      const a = 2;
+      const b = 3;
+      const resultado = 6;
+      expect(calculadora.multiplicacao(a, b)).toBe(resultado);
     });
   });
 
-  describe('divisao', () => {
+  describe('divisao-fact', () => {
     test('deve dividir dois números corretamente', () => {
-      expect(calculadora.divisao(6, 3)).toBe(2);
+      const a = 6;
+      const b = 3;
+      const resultado = 2;
+      expect(calculadora.divisao(a, b)).toBe(resultado);
+    });
+
+    test('deve lançar um erro ao dividir por zero', () => {
+      expect(() => {
+        calculadora.divisao(10, 0);
+      }).toThrow('Divisão por zero não é permitida.');
+    });
+  });
+  
+  describe('soma-theory', () => {
+    test.each([
+      [2, 3, 5],
+      [1, 1, 2],
+      [0, 0, 0]
+    ])('deve retornar %i quando somado %i e %i', (a, b, expected) => {
+      const result = calculadora.soma(a, b);
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe('subtracao-theory', () => {
+    test.each([
+      [5, 3, 2],
+      [1, 1, 0],
+      [0, 0, 0]
+    ])('deve retornar %i quando subtraído %i de %i', (a, b, expected) => {
+      const result = calculadora.subtracao(a, b);
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe('multiplicacao-theory', () => {
+    test.each([
+      [2, 3, 6],
+      [1, 1, 1],
+      [0, 0, 0]
+    ])('deve retornar %i quando multiplicado %i por %i', (a, b, expected) => {
+      const result = calculadora.multiplicacao(a, b);
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe('divisao-theory', () => {
+    test.each([
+      [6, 3, 2],
+      [2, 1, 2],
+      [0, 1, 0]
+    ])('deve retornar %i quando dividido %i por %i', (a, b, expected) => {
+      const result = calculadora.divisao(a, b);
+      expect(result).toBe(expected);
     });
 
     test('deve lançar um erro ao dividir por zero', () => {
